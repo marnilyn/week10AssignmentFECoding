2966f0ef737509fb770898e4266ff7e7bce8047a
Marnilyn Ramirez <You@example.com>
2023-08-19 18:43:15 -0400

    finalized html and js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+// Using HTML, Bootstrap, and JavaScript create a single page website that contains the following:
+// A Bootstrap styled table representing your choice of data.
+// A Bootstrap styled form that allows a user to add a new row to the table when clicking on submit.
 
-let btn = document.getElementById("myButton")
+let id = 0;
 
-const addAnotherOne = (event) => {
-    let newEle = document.createElement("div")
-    newEle.innerHTML = "ANOTHER ONE"
+document.getElementById('submit').addEventListener('click', () => {
+    let table = document.getElementById('list');
+    let row = table.insertRow(3);//this will insert the new entries to the 3rd row of the table
+    row.insertCell(0).innerHTML = document.getElementById('firstName').value;
+    row.insertCell(1).innerHTML = document.getElementById('lastName').value;
+    row.insertCell(2).innerHTML = document.getElementById('item').value;
+})//these are the values created
 
-    document.body.appendChild(newEle)
-}
-
-btn.addEventListener('click', addAnotherOne)
-
-btn.addEventListener('mouseover', addAnotherOne)
+myForm.style.backgroundColor = 'grey'
+myForm.style.color = 'white'
+myForm.style.display = 'flex'
+myForm.style.flexDirection = 'column'
